cb48df296b3b05ac46da707484595ba7f8acf464
devium335 <108692136+devium335@users.noreply.github.com>
2024-08-14 20:31:23 +0300

    fix imports

--- src/app/api/lock/route.ts
+++ src/app/api/lock/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { prisma } from "@/lib/prisma";
+import { prisma } from "../../lib/prisma";
 import { customAlphabet } from "nanoid";
 import CryptoJS from "crypto-js";
 
